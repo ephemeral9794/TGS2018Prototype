b94adb08b9a201e7b3fd897b3300983c4df4f707
はかなや <kthm9794.hiro@gmail.com>
2018-06-16 14:54:29 +0900

    Add Script and Resources

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	// Unity Editor設定値
+	[SerializeField]
+	protected Slider hpSlider;
+
+	// 内部変数
+	protected int hp;
+
+	// Use this for initialization
+	void Start () {
+		hp = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
