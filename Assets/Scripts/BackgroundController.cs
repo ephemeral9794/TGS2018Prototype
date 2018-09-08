92db31924068c9b4bd948b62e542ec8f1914100d
はかなや <kthm9794.hiro@gmail.com>
2018-09-08 14:16:37 +0900

    背景スクロール修正、ゴールとクリアを設定

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
-	[SerializeField, Range(-3.0f, 0.0f)]
-	private float speed = 0.1f;
+	[SerializeField, Range(-1.0f, 0.0f)]
+	private float speed = -0.1f;
+	[SerializeField]
+	private Player player;
 
 	// Update is called once per frame
 	void Update () {
 		if (Player.Enabled) {
-			transform.Translate(-0.0f, speed, 0);
+			float sp = (player.Speed / 60.0f) * speed;
+			/*transform.Translate(0.0f, sp, 0);
 			if( transform.position.y < -10.3f ) {
 				transform.position = new Vector3(0.0f, 10.3f, 90.3f);
-			}
+			}*/
+			float scroll = Mathf.Repeat (Time.time * 0.2f, 1);
+			Vector2 offset = new Vector2 (0, scroll);
+			GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
 		}
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,17 +12,27 @@ public class Player : MonoBehaviour {
 	[SerializeField]
 	private Slider hpSlider;
 	[SerializeField]
+	private Text hpText;
+	[SerializeField]
 	private Text gameOverText;
+	[SerializeField]
+	private Text gameClearText;
 	[SerializeField]
 	private Text SpeedText;
 	[SerializeField]
 	private Text TimeText;
+	[SerializeField]
+	private Slider distanceSlider;
 	/*[SerializeField]
 	private Animator hitAnimator;*/
 	//[SerializeField, Range(1, 100)]
 	//private int damage = 10;
 	[SerializeField]
 	private float duration = 2.0f;
+	[SerializeField]
+	private float goalDistance = 10.0f;
+	[SerializeField]
+	private int hpMax = 100;
 
 	// 内部変数
 	private int hp;
@@ -30,11 +40,17 @@ public class Player : MonoBehaviour {
 	private float timeDuration;
 	private int speed;
 	private float timeElapsed;
+	private float distance;
+	private float goal;
 
 	// プロパティ
 	public int HP {
 		get { return hp; }
-		set { hp = value > 100 ? 100 : value < 0 ? 0 : value; hpSlider.value = hp; }
+		set {
+			hp = (value > hpMax) ? hpMax : (value < 0) ? 0 : value;
+			hpSlider.value = hp;
+			hpText.text = string.Format("{0:##0}/{1}", hp, hpMax);
+		}
 	}
 	/*public int Damage {
 		get { return damage; }
@@ -56,7 +72,14 @@ public class Player : MonoBehaviour {
 		set {
 			timeElapsed = value;
 			int minite = (int)Math.Floor(timeElapsed / 60.0f);
-			TimeText.text = string.Format("{0:#0}:{1:00.##}", minite, timeElapsed);
+			TimeText.text = string.Format("{0:#0}:{1:00.00}", minite, timeElapsed);
+		}
+	}
+	public float Distance {
+		get { return distance; }
+		set {
+			distance = (value >= goal) ? goal : value;
+			distanceSlider.value = (distance / goal);
 		}
 	}
 	/*public GameObject HitAttack {
@@ -66,13 +89,17 @@ public class Player : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Enabled = true;
-		hp = 100;
+		hp = hpMax;
 		hpSlider.value = hp;
 		speed = 0;
 		SpeedText.text = string.Format("{0}km/h", speed);
 		timeElapsed = 0.0f;
 		TimeText.text = string.Format("{0:0.##}", timeElapsed);
 		gameOverText.enabled = false;
+		gameClearText.enabled = false;
+		distance = 0.0f;
+		distanceSlider.value = 0.0f;
+		goal = goalDistance;// * 3600.0f * Time.deltaTime;
 		//hit = transform.Find("Hit").gameObject;
 		//hit.GetComponent<BoxCollider2D>().enabled = false;
 		//hit.GetComponent<Image>().enabled = false;
@@ -101,6 +128,19 @@ public class Player : MonoBehaviour {
 
 			}
 		}*/
+		if (Enabled) {
+			float ps = Speed / 3600.0f;// 秒速
+			float fps = 1.0f / Time.deltaTime; // fps
+			float pf = ps / fps; // フレーム速
+			//Debug.Log("speed     : " + speed);
+			//Debug.Log("per frame : " + pf);
+			//Debug.Log("distance  : " + Distance);
+			Distance += pf;
+			if (Distance >= goal) {
+				gameClearText.enabled = true;
+				Enabled = false;
+			}
+		}
 
 		if (Enabled)
 			TimeElapsed += Time.deltaTime;
